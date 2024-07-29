124632211daf88c1dd324bd6a70d327f51804aa0
maxzdosreis <maxzdosreis@gmail.com>
2024-07-29 17:12:49 -0300

    First commit

---
+++ src/main/java/dio/lab_api_rest/LabApiRestApplication.java
@@ -0,0 +1,13 @@
+package dio.lab_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/lab_api_rest/LabApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.lab_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
